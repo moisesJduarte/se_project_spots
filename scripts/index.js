1e5dd789205ca1f609221fb4389b83a1d5e6b8d2
Moises Duarte <gladmd19@gmail.com>
2025-03-25 18:14:36 -0400

    modal functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -34,6 +34,23 @@ const initialCards = [
     }
 
 
-]
+];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editModal = document.querySelector("#edit-modal");
+const editModalCloseButton = editModal.querySelector(".modal__close-btn");
+
+
+function openModal() {
+    editModal.classList.add("modal_opened");
+
+}
+
+function closeModal() {
+    editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+editModalCloseButton.addEventListener("click", closeModal);
+
