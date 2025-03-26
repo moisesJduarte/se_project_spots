3ccc4a481b37a29a27f7e1ebcfd1c618e5263ef1
Moises Duarte <gladmd19@gmail.com>
2025-03-26 17:29:24 -0400

    form adding querySelector

--- scripts/index.js
+++ scripts/index.js
@@ -37,12 +37,18 @@ const initialCards = [
 ];
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description")
+
 
 const editModal = document.querySelector("#edit-modal");
 const editModalCloseButton = editModal.querySelector(".modal__close-btn");
-
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
 
 function openModal() {
+    editModalNameInput.value = profileName.textContent;
+    editModalDescriptionInput.value = profileDescription.textContent;
     editModal.classList.add("modal_opened");
 
 }
