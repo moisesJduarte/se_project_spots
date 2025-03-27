0d8a12c786f677a5b4bccd7b344610f4a7c9b434
Moises Duarte <gladmd19@gmail.com>
2025-03-27 18:16:41 -0400

    Refactor: Separate data from logic and improve code structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,24 @@
-const initialCards = [
-    {
-        name: "Val Thorens",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
-    }
-    ,
-    {
-        name: "Restaurant terrace",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
-    }
-    ,
-    {
-        name: "An outdoor cafe",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
-    }
-    ,
-    {
-        name: "A very long bridge, over the forest and through the trees",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
-    }
-    ,
+import { initialCards } from "./cards.js";
 
-    {
-        name: "Tunnel with morning light",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
-    }
-
-
-    ,
-
-    {
-        name: "Mountain house",
-        link: " https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-    }
-
-
-];
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-
-
 const editModal = document.querySelector("#edit-modal");
-const editFormElement = editModal.querySelector(".modal__form");
-
+const editFormElement = document.forms["edit-form"];
 const editModalCloseButton = editModal.querySelector(".modal__close-btn");
-const editModalNameInput = editModal.querySelector("#profile-name-input");
-const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
+
+
+const { "profile-name-input": editModalNameInput, "profile-description-input": editModalDescriptionInput } = editFormElement.elements;
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
 
 function openModal() {
     editModalNameInput.value = profileName.textContent;
     editModalDescriptionInput.value = profileDescription.textContent;
     editModal.classList.add("modal_opened");
-
 }
 
 function closeModal() {
@@ -66,20 +32,12 @@ function handleEditFormSubmit(evt) {
     closeModal();
 }
 
-
-const cardTemplate = document.querySelector("#card-template");
-const cardsList = document.querySelector(".cards__list")
-
-
 function getCardElement(data) {
     const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
-
-
     const cardNameEl = cardElement.querySelector(".card__title");
+    const cardImage = cardElement.querySelector(".card__image");
 
     cardNameEl.textContent = data.name;
-
-    const cardImage = cardElement.querySelector(".card__image");
     cardImage.src = data.link;
     cardImage.alt = data.name;
 
@@ -89,11 +47,10 @@ function getCardElement(data) {
 
 profileEditButton.addEventListener("click", openModal);
 editModalCloseButton.addEventListener("click", closeModal);
-editFormElement.addEventListener("submit", handleEditFormSubmit)
+editFormElement.addEventListener("submit", handleEditFormSubmit);
 
-for (let i = 0; i < initialCards.length; i++) {
 
-    const cardElement = getCardElement(initialCards[i]);
+initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
     cardsList.append(cardElement);
-
-} 
+});
