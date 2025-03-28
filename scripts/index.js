35715fe87255bfee6a31b1d67ff6d28b572b9d83
Moises Duarte <gladmd19@gmail.com>
2025-03-28 16:40:18 -0400

    fix: index.js & html file

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,3 @@
-import { initialCards } from "./cards.js";
-
-
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
@@ -8,12 +5,8 @@ const editModal = document.querySelector("#edit-modal");
 const editFormElement = document.forms["edit-form"];
 const editModalCloseButton = editModal.querySelector(".modal__close-btn");
 
-
-const { "profile-name-input": editModalNameInput, "profile-description-input": editModalDescriptionInput } = editFormElement.elements;
-
-const cardTemplate = document.querySelector("#card-template");
-const cardsList = document.querySelector(".cards__list");
-
+const editModalNameInput = editFormElement.elements["profile-name-input"];
+const editModalDescriptionInput = editFormElement.elements["profile-description-input"];
 
 function openModal() {
     editModalNameInput.value = profileName.textContent;
@@ -32,25 +25,6 @@ function handleEditFormSubmit(evt) {
     closeModal();
 }
 
-function getCardElement(data) {
-    const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
-    const cardNameEl = cardElement.querySelector(".card__title");
-    const cardImage = cardElement.querySelector(".card__image");
-
-    cardNameEl.textContent = data.name;
-    cardImage.src = data.link;
-    cardImage.alt = data.name;
-
-    return cardElement;
-}
-
-
 profileEditButton.addEventListener("click", openModal);
 editModalCloseButton.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
-
-
-initialCards.forEach((cardData) => {
-    const cardElement = getCardElement(cardData);
-    cardsList.append(cardElement);
-});
