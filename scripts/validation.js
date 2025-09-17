b35f85200c3db8151c253b3a2186421f7eacd9bd
Moises Duarte <gladmd19@gmail.com>
2025-09-16 23:05:19 -0400

    fix typo in validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -9,6 +9,7 @@ const showInputError = (formEl, inputEl, errorMsg) => {
 const hideInputError = (formEl, inputEl, errorMsg) => {
     const errorMsgEl = formEl.querySelector(`.${inputEl.id}-error`);
     errorMsgEl.textContent = errorMsg;
+    inputEl.classList.remove("modal__input_type_error");
 };
 
 
@@ -18,25 +19,40 @@ const checkInputValidity = (formEl, inputEl) => {
     } else {
         hideInputError(formEl, inputEl);
     }
+};
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((input) => {
+        return !input.validity.valid;
+    });
+};
+
+
+const toggleButtonState = (inputList, buttonEl) => {
+    if (hasInvalidInput(inputList)) {
+        buttonEl.disabled = true;
+        buttonEl.classList.add("modal__submit-btn_disabled");
+
+    } else {
+        buttonEl.disabled = false;
+        buttonEl.classList.remove("modal__submit-btn_disabled");
+
+    }
 }
+
 
 const setEventListeners = (formEl) => {
     const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
-    const buttonElement = formEl.querySelector(".modal__button");
-
-
-
-
+    const buttonElement = formEl.querySelector(".modal__submit-btn");
     // toggleButtonState(inputList, buttonElement);
 
     inputList.forEach((inputElement) => {
         inputElement.addEventListener("input", function () {
             checkInputValidity(formEl, inputElement);
-            // toggleButtonState(inputList, buttonElement);
+            toggleButtonState(inputList, buttonElement);
         });
     });
 };
-
 
 const enableValidation = () => {
     const formList = Array.from(document.querySelectorAll(".modal__form"));
